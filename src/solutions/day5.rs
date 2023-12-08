0c27377b610ccb650119737e790a58e92622ba19
swz-git <contact@swz.works>
2023-12-08 21:00:04 +0100

    split range progress day5 part2

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -75,70 +75,77 @@ pub fn part1(input: &Almanac) -> u32 {
     lowest
 }
 
+fn split_range(range: &Range<u32>, map_part: &MapPart) -> Option<Vec<Range<u32>>> {
+    let map_part_range = map_part.1..(map_part.1 + map_part.2);
+    if range.end < map_part_range.start || range.start > map_part_range.end {
+        // out of range
+        None
+    } else if range.start <= map_part_range.start && map_part_range.end <= range.end {
+        // [>
+        Some(vec![
+            (range.start..map_part.1),
+            (map_part.0..(map_part.0 + map_part.2)),
+            ((map_part.1 + map_part.2)..range.end),
+        ])
+    } else if map_part_range.start <= range.start && range.end <= map_part_range.end {
+        // <]
+        Some(vec![
+            ((range.start - map_part.1 + map_part.0)..(range.end - map_part.1 + map_part.0)),
+        ])
+    } else if map_part_range.start <= range.start && map_part_range.end <= range.end {
+        //  \
+        // //
+        // \
+        Some(vec![
+            ((range.start - map_part.1 + map_part.0)..(map_part.0 + map_part.2)),
+            ((map_part.1 + map_part.2)..range.end),
+        ])
+    } else {
+        // /
+        // \\
+        //  /
+        Some(vec![
+            (range.start..map_part.1),
+            (map_part.0..(range.end - map_part.1 + map_part.0)),
+        ])
+    }
+}
+
 #[aoc(day5, part2)]
 pub fn part2(input: &Almanac) -> u32 {
-    let mut lowest = u32::MAX;
-
     let mut ranges: Vec<Range<u32>> = input
         .seed_info
         .chunks(2)
         .map(|chunk| chunk[0]..(chunk[0] + chunk[1]))
         .collect();
 
-    for map in &input.maps {
-        for map_part in &map.0 {
-            let map_part_range = map_part.1..(map_part.1 + map_part.2);
-            ranges = ranges
-                .iter()
-                .flat_map(|range| {
-                    // https://filebin.swz.works/api/file/F55fT1iwaI4 \/
-                    let to_return = if range.end < map_part_range.start
-                        || range.start > map_part_range.end
-                    {
-                        // out of range
-                        vec![range.clone()]
-                    } else if range.start <= map_part_range.start && map_part_range.end <= range.end
-                    {
-                        // [>
-                        vec![
-                            (range.start..map_part.1),
-                            (map_part.0..(map_part.0 + map_part.2)),
-                            ((map_part.1 + map_part.2)..range.end),
-                        ]
-                    } else if map_part_range.start <= range.start && range.end <= map_part_range.end
-                    {
-                        // <]
-                        vec![
-                            ((range.start - map_part.1 + map_part.0)
-                                ..(range.end - map_part.1 + map_part.0)),
-                        ]
-                    } else if map_part_range.start <= range.start && map_part_range.end <= range.end
-                    {
-                        //  \
-                        // //
-                        // \
-                        vec![
-                            ((range.start - map_part.1 + map_part.0)..(map_part.0 + map_part.2)),
-                            ((map_part.1 + map_part.2)..range.end),
-                        ]
-                    } else {
-                        // /
-                        // \\
-                        //  /
-                        vec![
-                            (range.start..map_part.1),
-                            (map_part.0..(range.end - map_part.1 + map_part.0)),
-                        ]
-                    };
+    for (i, og_range) in ranges.clone().iter().enumerate() {
+        let mut to_insert = vec![og_range.clone()];
 
-                    println!("{:?} became {:?} in stage {:?}", range, to_return, map.0);
-
-                    to_return
-                })
-                .collect();
+        for (map_i, map) in input.maps.iter().enumerate() {
+            println!("map {}: {:?}", map_i, map.0);
+            for (j, range) in to_insert.clone().iter().enumerate() {
+                let mut last_map_part = None;
+                let Some(splitted) = map.0.iter().find_map(|map_part| {
+                    last_map_part = Some(map_part);
+                    split_range(&range, map_part)
+                }) else {
+                    break;
+                };
+                dbg!(range, last_map_part, &splitted);
+                // replace with multiple
+                to_insert.remove(j);
+                to_insert.splice(j..j, splitted);
+            }
         }
-        println!()
+        dbg!(&to_insert, "-------------------------");
+
+        // replace with multiple
+        ranges.remove(i);
+        ranges.splice(i..i, to_insert);
     }
 
-    lowest
+    dbg!(ranges);
+
+    todo!()
 }
