68473b702859bb087f9a9dde1de57491f1f4b434
swz-git <contact@swz.works>
2023-12-11 23:45:44 +0100

    day 9

---
+++ src/solutions/day9.rs
@@ -0,0 +1,60 @@
+// i need to wrap the vecs for cargo-aoc to be happy
+pub struct SensorOutput(Vec<Vec<i32>>);
+
+#[aoc_generator(day9)]
+pub fn generator(input: &str) -> SensorOutput {
+    SensorOutput(
+        input
+            .lines()
+            .map(|x| x.split(" ").map(|x| x.parse().unwrap()).collect())
+            .collect(),
+    )
+}
+
+#[aoc(day9, part1)]
+pub fn part1(input: &SensorOutput) -> i32 {
+    let mut sums = 0;
+
+    for value_history in &input.0 {
+        let mut value_history = value_history.clone();
+        let mut vh_sum = *value_history.last().unwrap();
+
+        // loop until value_history is an empty vec of zeros
+        while value_history.iter().filter(|x| **x != 0).next().is_some() {
+            let mut swap = vec![];
+            for i in 1..value_history.len() {
+                swap.push(value_history[i] - value_history[i - 1])
+            }
+            vh_sum += swap.last().unwrap();
+            value_history = swap;
+        }
+
+        sums += vh_sum
+    }
+
+    sums
+}
+
+#[aoc(day9, part2)]
+pub fn part2(input: &SensorOutput) -> i32 {
+    let mut sums = 0;
+
+    for value_history in &input.0 {
+        let mut value_history = value_history.clone();
+        let mut values = vec![*value_history.first().unwrap()];
+
+        // loop until value_history is an empty vec of zeros
+        while value_history.iter().filter(|x| **x != 0).next().is_some() {
+            let mut swap = vec![];
+            for i in 1..value_history.len() {
+                swap.push(value_history[i] - value_history[i - 1])
+            }
+            values.push(*swap.first().unwrap());
+            value_history = swap;
+        }
+
+        sums += values.iter().rev().fold(0, |acc, x| x - acc)
+    }
+
+    sums
+}
